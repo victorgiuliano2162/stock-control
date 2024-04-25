688bd29912a4c265d10ce0a76f112bec29afcc64
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-25 12:19:56 -0300

    add cookie service provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CardModule } from "primeng/card";
 import { InputTextModule } from "primeng/inputtext"
 import { ButtonModule } from "primeng/button"
 import { ToastModule } from "primeng/toast"
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,10 @@ import { ToastModule } from "primeng/toast"
     ButtonModule,
     ToastModule,
   ],
-  providers: [],
+  providers: [
+    CookieService,
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
