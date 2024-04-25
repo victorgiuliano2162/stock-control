abb8b40c78ca08f2d7974156edbc791fbe545e9d
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-04-25 09:40:23 -0300

    add angular dinamic forms

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
-  
+
+  loginForm = this.formBuilder.group({
+    email: ["", Validators.required],
+    password: ["", Validators.required] 
+
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ["", Validators.required],
+    email: ["", Validators.required],
+    password: ["", Validators.required],
+  })
+
+  constructor(private formBuilder: FormBuilder) {
+    //Para utilizá-lo é preciso importar o ReativeFormsModule no app.module.ts
+  }
+
+  onSubmitLoginForm(): void {
+    console.log("Submit funciona", this.loginForm.value);
+  }
+
+  onSubmitSignUpForm(): void {
+    console.log("Sign up funciona", this.signUpForm.value);
+  }
 }
