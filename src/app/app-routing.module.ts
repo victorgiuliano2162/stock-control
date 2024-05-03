a937f46917c8584401c741f56a6dc1b1a6ec4964
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-05-03 00:31:31 -0300

    added dashboard route with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboadHomeComponent } from './modules/dashboard/page/dashboad-home/dashboad-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: "home",
     component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    //implementação do lazy loading
+    loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
   }
 ];
 
